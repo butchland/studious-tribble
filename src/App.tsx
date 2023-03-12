1674fb458f326b211f827d062472c95680d27762
Butch Landingin <butchland@gmail.com>
2023-03-12 14:00:20 +0800

    fix: Add subpath VITE_HOME_PATH to link paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
 import LoadingOrError from 'components/LoadingOrError'
 import type { ReactElement } from 'react'
 import { lazy, Suspense } from 'react'
@@ -7,12 +8,13 @@ const Gallery = lazy(async () => import('pages/Gallery'))
 const Details = lazy(async () => import('pages/Details'))
 
 export default function App(): ReactElement {
+	const { VITE_HOME_PATH } = import.meta.env
 	return (
 		<BrowserRouter>
 			<Suspense fallback={<LoadingOrError />}>
 				<Routes>
-					<Route path='/' element={<Gallery />} />
-					<Route path=':fruitName' element={<Details />} />
+					<Route path={`${VITE_HOME_PATH}/`} element={<Gallery />} />
+					<Route path={`${VITE_HOME_PATH}/:fruitName`} element={<Details />} />
 				</Routes>
 			</Suspense>
 		</BrowserRouter>
